086900ece5afc27911e23862fc67ab5d6d5545fa
lin onetwo <linonetwo012@gmail.com>
2025-07-21 19:23:07 +0800

    chore: config eslint

---
+++ intention-tower-game/eslint.config.mjs
@@ -0,0 +1,28 @@
+import tidgiConfig from 'eslint-config-tidgi';
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default [
+  ...tidgiConfig,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: ['./*.js', './*.mjs','./*.*.js', './*.*.ts', './*.*.mjs'],
+        },
+        tsconfigRootDir: __dirname,
+      },
+    },
+    rules: {},
+  },
+  {
+    files: ['**/*.test.ts', '**/*.test.tsx', '**/*.spec.ts', '**/*.spec.tsx'],
+    rules: {
+      '@typescript-eslint/unbound-method': 'off',
+      'unicorn/prevent-abbreviations': 'off',
+    },
+  },
+];
